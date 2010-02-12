3493450fe416959ea27a2a88f7b9e3b078800328
organicveggie <organicveggie@c5505b5e-179c-11df-945e-a3343a1c6be9>
2010-02-12 06:18:43 +0000

    Fixed compile and launch shell script templates.

--- generators/gwt_client/templates/app/gwt/shell_template.rb
+++ generators/gwt_client/templates/app/gwt/shell_template.rb
@@ -1,4 +1,8 @@
 #!/bin/sh
 APPDIR=`dirname $0`;
 VENDOR_LIBS="$APPDIR/lib/gwt-rest.jar:$APPDIR/lib/gwtx.jar" # append custom entries here
-java -XstartOnFirstThread -cp "$APPDIR/src:$VENDOR_LIBS:$APPDIR/bin:<%= gwt_home %>/gwt-user.jar:<%= gwt_home %>/gwt-dev-<%= gwt_dev_platform %>.jar" com.google.gwt.dev.GWTShell -out "<%= RAILS_ROOT %>/public/gwt" "$@" -noserver -port 3000 <%= file_name %>;
+
+GWT_HOME="<%= gwt_home %>"
+GWT_CP="$GWT_HOME/gwt-user.jar:$GWT_HOME/gwt-dev.jar"
+
+java -cp "$APPDIR/src:$VENDOR_LIBS:$APPDIR/bin:$GWT_CP" com.google.gwt.dev.DevMode -out "<%= RAILS_ROOT %>/public/gwt" "$@" -noserver -port 3000 <%= file_name %>
