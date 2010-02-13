42dbb85f2c880fb15561be736baa381c8a1b4ef6
organicveggie@gmail.com <organicveggie@gmail.com@c5505b5e-179c-11df-945e-a3343a1c6be9>
2010-02-13 05:30:18 +0000

    Removed additional references to GWTx.

--- trunk/generators/gwt_client/templates/app/gwt/shell_template.rb
+++ trunk/generators/gwt_client/templates/app/gwt/shell_template.rb
@@ -1,8 +1,8 @@
 #!/bin/sh
 APPDIR=`dirname $0`;
-VENDOR_LIBS="$APPDIR/lib/gwt-rest.jar:$APPDIR/lib/gwtx.jar" # append custom entries here
+VENDOR_LIBS="$APPDIR/lib/gwt-rest.jar" # append custom entries here
 
 GWT_HOME="<%= gwt_home %>"
 GWT_CP="$GWT_HOME/gwt-user.jar:$GWT_HOME/gwt-dev.jar"
 
-java -cp "$APPDIR/src:$VENDOR_LIBS:$APPDIR/bin:$GWT_CP" com.google.gwt.dev.DevMode -out "<%= RAILS_ROOT %>/public/gwt" "$@" -noserver -port 3000 <%= file_name %>
+java -cp "$APPDIR/src:$VENDOR_LIBS:$APPDIR/bin:$GWT_CP" com.google.gwt.dev.DevMode -war "<%= RAILS_ROOT %>/public/gwt" "$@" -noserver -port 3000 <%= file_name %>
